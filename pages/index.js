9dda6dcd1f66461cdd56fbc71a6efbaef47ad144
niina58 <KTC23A31C0007@edu.kyoto-tech.ac.jp>
2023-11-24 16:29:42 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Header from '../components/header'
-import Hero from '../components/hero'
-import Footer from '../components/footer'
+import Hero from '@/components/hero'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-const subtitle = 'アウトプットしていくサイト'
 
   return (
-    <>
-      <Header />
-      
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
+    <Hero
+      title="CUBE"
+      subtitle="アウトプットしていくサイト"
+      imageOn
+    />
   )
 }
