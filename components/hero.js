99590f5ba5df57d430f558ced76c16faa4a2a748
niina58 <KTC23A31C0007@edu.kyoto-tech.ac.jp>
2023-11-24 16:29:24 +0900

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,13 @@
-export default function Hero() {
+import styles from 'styles/hero.module.css'
+
+export default function Hero({ title, subtitle, imageOn = false }) {
     return (
         <div>
-            <h1>CUBE</h1>
-            <p>アウトプットしていくサイト</p>
+            <div>
+                <h1>{title}</h1>
+                <p>{subtitle}</p>
+            </div>
+            {imageOn && <figure> [画像] </figure>}
         </div>
     )
 }
