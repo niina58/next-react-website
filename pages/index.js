1df4bb4cd1347f0c28cde3deb56d4171fe327e1c
niina58 <KTC23A31C0007@edu.kyoto-tech.ac.jp>
2023-12-15 16:28:55 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Contaiter from 'components/container'
+import Container from "components/container";
 import Hero from 'components/hero'
 
 const inter = Inter({ subsets: ['latin'] })
