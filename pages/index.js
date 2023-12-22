0b653301fbfbc800fd54dace18ddf4760dd9e913
niina58 <131966572+niina58@users.noreply.github.com>
2023-12-22 16:32:19 +0900

    a

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 import Container from "components/container";
 import Hero from 'components/hero'
 
